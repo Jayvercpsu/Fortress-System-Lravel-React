9c4e69be5529225fb7e2e2979e4569948e0e447b
joshxb2 <algadipej962@gmail.com>
2026-08-18 11:38:27 +0800

    feat: add additional user roles to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,37 @@ class DatabaseSeeder extends Seeder {
             'password' => Hash::make('password'),
             'role'     => 'head_admin',
         ]);
+
+        User::updateOrCreate([
+            'email'    => 'admin@buildbooks.com',
+        ], [
+            'fullname' => 'Administrator',
+            'password' => Hash::make('password'),
+            'role'     => 'admin',
+        ]);
+
+        User::updateOrCreate([
+            'email'    => 'hr@buildbooks.com',
+        ], [
+            'fullname' => 'HR Personnel',
+            'password' => Hash::make('password'),
+            'role'     => 'hr',
+        ]);
+
+        User::updateOrCreate([
+            'email'    => 'foreman@buildbooks.com',
+        ], [
+            'fullname' => 'Foreman',
+            'password' => Hash::make('password'),
+            'role'     => 'foreman',
+        ]);
+
+        User::updateOrCreate([
+            'email'    => 'designer@buildbooks.com',
+        ], [
+            'fullname' => 'Designer',
+            'password' => Hash::make('password'),
+            'role'     => 'designer',
+        ]);
     }
 }
