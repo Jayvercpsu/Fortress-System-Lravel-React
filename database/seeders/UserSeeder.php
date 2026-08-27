98ce6b30fa0e66ed4e45741d85d6e10359b63896
joshxb2 <algadipej962@gmail.com>
2026-08-27 13:19:18 +0800

    feat: refactor database seeding by moving user creation logic to UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,58 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder {
+    public function run(): void {
+        User::updateOrCreate([
+            'email'    => 'masteradmin@buildbooks.com',
+        ], [
+            'fullname' => 'Master Administrator',
+            'password' => Hash::make('password'),
+            'role'     => 'master_admin',
+        ]);
+
+        User::updateOrCreate([
+            'email'    => 'headadmin@buildbooks.com',
+        ], [
+            'fullname' => 'Head Administrator',
+            'password' => Hash::make('password'),
+            'role'     => 'head_admin',
+        ]);
+
+        User::updateOrCreate([
+            'email'    => 'admin@buildbooks.com',
+        ], [
+            'fullname' => 'Administrator',
+            'password' => Hash::make('password'),
+            'role'     => 'admin',
+        ]);
+
+        User::updateOrCreate([
+            'email'    => 'hr@buildbooks.com',
+        ], [
+            'fullname' => 'HR Personnel',
+            'password' => Hash::make('password'),
+            'role'     => 'hr',
+        ]);
+
+        User::updateOrCreate([
+            'email'    => 'foreman@buildbooks.com',
+        ], [
+            'fullname' => 'Foreman',
+            'password' => Hash::make('password'),
+            'role'     => 'foreman',
+        ]);
+
+        User::updateOrCreate([
+            'email'    => 'designer@buildbooks.com',
+        ], [
+            'fullname' => 'Designer',
+            'password' => Hash::make('password'),
+            'role'     => 'designer',
+        ]);
+    }
+}
